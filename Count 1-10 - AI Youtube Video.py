a0069bc3c9c263817c8c7a0ae00a773ef06c6cd9
Cowne <ashcowne@gmail.com>
2024-03-12 13:52:18 -0400

    Added for loop called count printing numbers 5-20

---
+++ Count 1-10 - AI Youtube Video.py
@@ -0,0 +1,9 @@
+# Programer: Ash Cowne
+# Date: 3.12.2024
+# Program: Count 1-10
+# Respourse: https://youtu.be/jRAAaDll34Q?si=SZq8WSYzjrmuAoIA
+
+print("hi")
+
+for count in range(5, 21):
+    print(count)
