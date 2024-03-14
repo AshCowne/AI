06d38b0726864f3a6c55911a8656cd62989c9a00
Cowne <ashcowne@gmail.com>
2024-03-14 14:09:44 -0400

    Condence Printing Names Index ChatGPT Stable

--- PrintingNamesIndex.py
+++ PrintingNamesIndex.py
@@ -4,7 +4,5 @@
 # Resource: https://youtu.be/jRAAaDll34Q?si=SZq8WSYzjrmuAoIA
 
 names = ["Corey", "Chris", "Travis", "Dave"]
-index = 0
-for name in names:
+for index, name in enumerate(names):
     print(index, name)
-    index += 1
